c30fee154e1808f59908b69d2d0dd37b76db5865
Martin Rabaglia <tinchoforever@gmail.com>
2013-09-29 12:33:54 -0300

    mas cositas

--- application/views/layouts/admin.blade.php
+++ application/views/layouts/admin.blade.php
@@ -1,30 +1,45 @@
-<!DOCTYPE html>
+<!doctype html>
+<!-- paulirish.com/2008/conditional-stylesheets-vs-css-hacks-answer-neither/ -->
+<!--[if lt IE 7]> <html class="no-js ie6 oldie" lang="en"> <![endif]-->
+<!--[if IE 7]>    <html class="no-js ie7 oldie" lang="en"> <![endif]-->
+<!--[if IE 8]>    <html class="no-js ie8 oldie" lang="en"> <![endif]-->
+<!--[if IE 9]>    <html class="no-js ie9" lang="en"> <![endif]-->
+<!-- Consider adding an manifest.appcache: h5bp.com/d/Offline -->
+<!--[if gt IE 9]><!-->
 <html xmlns:ng="http://angularjs.org">
+<html class="no-js" lang="en" itemscope itemtype="http://schema.org/Product"> <!--<![endif]-->
 <head>
-  <!--[if lte IE 8]>
-  <script>
-    document.createElement('ng-include');
-    document.createElement('ng-pluralize');
-    document.createElement('ng-view');
-    document.createElement('ng:include');
-    document.createElement('ng:pluralize');
-    document.createElement('ng:view');
-  </script>
-  <![endif]-->
+	<meta charset="utf-8">
+	<meta name="fragment" content="!" />
+	<!-- Use the .htaccess and remove these lines to avoid edge case issues.
+			 More info: h5bp.com/b/378 -->
+	<meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
 
-  <meta http-equiv="content-type" content="text/html; charset=UTF-8">
-  <meta name="apple-mobile-web-app-capable" content="yes">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no">
-  <title></title>
-  <meta name="description" content="">
-  <meta name="viewport" content="width=device-width">
-  <script src="js/frameworks/zepto.min.js"></script>
+	<title>Hackaton Litoral 2013</title>
+	<meta name="description" content="" />
+	<meta name="keywords" content="" />
+	<meta name="author" content="humans.txt">
+
+	<link rel="shortcut icon" href="favicon.png" type="image/x-icon" />
+
+	<!-- Facebook Metadata /-->
+	<meta property="fb:page_id" content="" />
+	<meta property="og:image" content="" />
+	<meta property="og:description" content=""/>
+	<meta property="og:title" content=""/>
+
+	<!-- Google+ Metadata /-->
+	<meta itemprop="name" content="">
+	<meta itemprop="description" content="">
+	<meta itemprop="image" content="">
+
+	<meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1">
 
   <script src="http://maps.googleapis.com/maps/api/js?sensor=false&amp;language=en"></script>
   <script src="http://ajax.googleapis.com/ajax/libs/jquery/1.9.1/jquery.min.js"></script>
 
   <!-- Place favicon.ico and apple-touch-icon.png in the root directory -->
-  <link rel="stylesheet" href="css/frameworks/gumby.css">
+  <link rel="stylesheet" href="css/gumby.css">
   <link rel="stylesheet" href="css/base.css">
   <link rel="stylesheet" href="http://cdn.leafletjs.com/leaflet-0.5.1/leaflet.css" />
 
@@ -32,8 +47,24 @@
 
 </head>
 <body ng-app="App">
-  <!-- Add your site or application content here -->
-  <div class="container">
+  <div class="navbar" id="nav1">
+		<div class="row">
+			<a class="toggle" gumby-trigger="#nav1 > .row > ul" href="#"><i class="icon-menu"></i></a>
+			<h3 class="four columns logo white">
+				#hackatonlitoral
+			</h3>
+			<ul class="eight columns">
+				<li><a href="#">Agenda</a></li>
+				<li><a href="#">Inscribite</a></li>
+				<li><a href="#">Subi tu idea!</a></li>
+				<li class="field white">
+					<i class="icon-facebook-circled icon-big"></i>
+					<i class="icon-twitter-circled icon-big"></i>
+					<i class="icon-tumblr-circled icon-big"></i>
+				</li>
+			</ul>
+		</div>
+	</div><div class="container">
     @yield('content')
   </div>
   <div class="footer">
@@ -48,15 +79,14 @@
   <script src="js/frameworks/angular.min.js"></script>
   <script src="js/frameworks/angular-resource.js"></script>
   <script src="js/frameworks/angular-google-maps.js"></script>
+  <script src="js/frameworks/angular-seo.js"></script>
 
   <script src="js/libs/moment.min.js"></script>
   <script src="js/libs/modernizr-2.0.6.min.js"></script>
   <script src="js/libs/gumby.min.js"></script>
 
   <script src="js/controllers/main.js"></script>
-  <script src="js/controllers/list.js"></script>
   <script src="js/services/libs/localStorage.js"></script>
-  <script src="js/services/logic/points.js"></script>
 
   <script src="js/app.js"></script>
 
--- public/js/app.js
+++ public/js/app.js
@@ -1,28 +1,14 @@
 
 'use strict';
-
-angular.module('App', ['App.services','App.controllers'])
+angular.module('App', ['seo','App.controllers'])
   .config(function ($routeProvider) {
     $routeProvider
       .when('/', {
         templateUrl: 'views/main.html',
         controller: 'main'
       })
-      .when('/gallery', {
-        templateUrl: 'views/art-list.html',
-        controller: 'list'
-      })
       .otherwise({
         redirectTo: '/'
       });
   });
 
-// angular.module('App.controllers', [])
-//   .controller('home', function () {
-//     $scope.property = true;
-//   });
-
-// angular.module('App', ['App.controllers'])
-//   .config(['$routeProvider', function($routeProvider) {
-//     $routeProvider.when('/', {templateUrl: 'partials/home.html', controller: 'home'});
-//   }]);
