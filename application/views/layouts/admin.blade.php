1e8ba6bd056269e92261ba4705b7eae231c59979
Bruno Lazzaro <brunomilestone@gmail.com>
2013-10-03 01:36:34 -0300

    V2
    ;

--- application/views/layouts/admin.blade.php
+++ application/views/layouts/admin.blade.php
@@ -9,38 +9,36 @@
 <html xmlns:ng="http://angularjs.org">
 <html class="no-js" lang="en" itemscope itemtype="http://schema.org/Product"> <!--<![endif]-->
 <head>
-	<meta charset="utf-8">
-	<meta name="fragment" content="!" />
-	<!-- Use the .htaccess and remove these lines to avoid edge case issues.
-			 More info: h5bp.com/b/378 -->
-	<meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
+  <meta charset="utf-8">
+  <meta name="fragment" content="!" />
 
-	<title>Hackaton Litoral 2013</title>
-	<meta name="description" content="Del 11 al 13 de Octubre nos juntamos en Santa Fe a generar soluciones innovadoras para los problemas de la ciudad." />
-	<meta name="keywords" content="Hackathon, desarrollo moviles,moviles ciudad,santa fe, innovacion, unl, fich, 2013, hackathon litoral" />
-	<meta name="robots" content="Index, All">
-	<meta name="robots" CONTENT="Index, Follow">
-	<meta name="revisit-after" content="10 days">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
 
-	<link rel="shortcut icon" href="favicon.png" type="image/x-icon" />
+  <title>Hackaton Litoral 2013</title>
+  <meta name="description" content="Del 11 al 13 de Octubre nos juntamos en Santa Fe a generar soluciones innovadoras para los problemas de la ciudad." />
+  <meta name="keywords" content="Hackathon, desarrollo moviles,moviles ciudad,santa fe, innovacion, unl, fich, 2013, hackathon litoral" />
+  <meta name="robots" content="Index, All">
+  <meta name="robots" CONTENT="Index, Follow">
+  <meta name="revisit-after" content="10 days">
 
-	<!-- Facebook Metadata /-->
-	<meta property="fb:page_id" content="" />
-	<meta property="og:image" content="http://fich.unl.edu.ar/hackathon2013/images/logo-hackathon-2013.jpg" />
-	<meta property="og:description" content="Del 11 al 13 de Octubre nos juntamos en Santa Fe a generar soluciones innovadoras para los problemas de la ciudad. Inscribite y se parte de la solucion!"/>
-	<meta property="og:title" content="#hackatonlitoral 2013"/>
+  <link rel="shortcut icon" href="favicon.png" type="image/x-icon" />
 
-	<!-- Google+ Metadata /-->
-	<meta itemprop="name" content="">
-	<meta itemprop="description" content="">
-	<meta itemprop="image" content="">
+  <!-- Facebook Metadata /-->
+  <meta property="fb:page_id" content="" />
+  <meta property="og:image" content="http://fich.unl.edu.ar/hackathon2013/images/logo-hackathon-2013.jpg" />
+  <meta property="og:description" content="Del 11 al 13 de Octubre nos juntamos en Santa Fe a generar soluciones innovadoras para los problemas de la ciudad. Inscribite y se parte de la solucion!"/>
+  <meta property="og:title" content="#hackatonlitoral 2013"/>
 
-	<meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1">
+  <!-- Google+ Metadata /-->
+  <meta itemprop="name" content="">
+  <meta itemprop="description" content="">
+  <meta itemprop="image" content="">
 
-  <script src="http://maps.googleapis.com/maps/api/js?sensor=false&amp;language=en"></script>
+  <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1">
+
   <script src="http://ajax.googleapis.com/ajax/libs/jquery/1.9.1/jquery.min.js"></script>
 
-  <!-- Place favicon.ico and apple-touch-icon.png in the root directory -->
+  <link href='http://fonts.googleapis.com/css?family=PT+Sans:300,400,600,700' rel='stylesheet' type='text/css'>
   <link rel="stylesheet" href="css/gumby.css">
   <link rel="stylesheet" href="css/base.css">
   <link rel="stylesheet" href="http://cdn.leafletjs.com/leaflet-0.5.1/leaflet.css" />
@@ -49,64 +47,45 @@
 
 </head>
 <body ng-app="App">
-  <div class="navbar" id="nav1">
-		<div class="row">
-			<a class="toggle" gumby-trigger="#nav1 > .row > ul" href="#"><i class="icon-menu"></i></a>
-			<h4 class="four columns logo white">
-				<a href="/#">#hackatonlitoral</a>
-			</h4>
-			<ul class="eight columns">
-				<li><a href="#/agenda">Agenda</a></li>
-				<li><a href="http://fich.unl.edu.ar/hackathon2013/inscripcion.php">Inscribite</a></li>
-				<li><a target="_blank" href="http://hacksf2013.hackdash.org/">Subi tu idea!</a></li>
-				<li class="icons field white">
-					<div class="row">
 
-							<a href="http://www.facebook.com/events/1450927081799187/" target="_blank">
-								<i class="icon-facebook-circled icon-big"></i></a>
+  <header>
+    <h1 class="logo">
+      <a href="/">#hackatonlitoral</a>
+    </h1>
+  </header>
 
+  <nav class="nav" id="nav1">
+    <div class="row">
+      <ul class="eight columns">
+        <li><a href="#/agenda">Agenda</a></li>
+        <li><a href="http://fich.unl.edu.ar/hackathon2013/inscripcion.php">Inscribite</a></li>
+        <li><a target="_blank" href="http://hacksf2013.hackdash.org/">Subi tu idea!</a></li>
+      </ul>
+    </div>
+  </nav>
 
-							<a href="http://twitter.com/hackatonlitoral" target="_blank">
-								<i class="icon-twitter-circled icon-big"></i>
-							</a>
-
-							<a href="http://tumblr.com/hackatonlitoral" target="_blank">
-								<i class="icon-tumblr-circled icon-big"></i>
-							</a>
-
-					</div>
-				</li>
-			</ul>
-		</div>
-	</div>
-	<div class="container">
+  <div class="container">
     @yield('content')
-  	</div>
+  </div>
 
-	  <div class="wrapper shaded" id="subfoot">
-	  	<section class="row">
-	      <div class="twelve columns centered">
-	      		<p>Copyright © Hackathon Litoral 2013.</p>
-	      		<div class="row">
-
-							<a href="http://www.facebook.com/events/1450927081799187/" target="_blank">
-								<i class="icon-facebook-circled icon-big"></i></a>
-
-
-							<a href="http://twitter.com/hackatonlitoral" target="_blank">
-								<i class="icon-twitter-circled icon-big"></i>
-							</a>
-
-							<a href="http://tumblr.com/hackatonlitoral" target="_blank">
-								<i class="icon-tumblr-circled icon-big"></i>
-							</a>
-
-					</div>
-
-	       </div>
-	  	</section>
-	  </div>
-
+  <div class="wrapper shaded" id="subfoot">
+    <section class="row">
+      <div class="twelve columns centered">
+          <p>Copyright © Hackathon Litoral 2013.</p>
+          <div class="row">
+            <a href="http://www.facebook.com/events/1450927081799187/" target="_blank"> 
+              <i class="icon-facebook-circled icon-big"></i>
+            </a>
+            <a href="http://twitter.com/hackatonlitoral" target="_blank">
+              <i class="icon-twitter-circled icon-big"></i>
+            </a>
+            <a href="http://tumblr.com/hackatonlitoral" target="_blank">
+              <i class="icon-tumblr-circled icon-big"></i>
+            </a>
+        </div>
+       </div>
+    </section>
+  </div>
 
   <!-- build:js scripts/scripts.js -->
 
@@ -119,17 +98,15 @@
   <script src="js/libs/modernizr-2.0.6.min.js"></script>
   <script src="js/libs/gumby.min.js"></script>
 
+  <script type="text/javascript"src="https://maps.googleapis.com/maps/api/js?key=AIzaSyD4ZXkREbaqNu06-W9cswKg3ZnI6TWrHLI&sensor=false"></script>
+  
   <script src="js/controllers/main.js"></script>
   <script src="js/controllers/agenda.js"></script>
   <script src="js/services/libs/localStorage.js"></script>
 
   <script src="js/app.js"></script>
 
-  <script src="http://cdn.leafletjs.com/leaflet-0.5.1/leaflet.js"></script>
-  <script src="http://code.jquery.com/jquery-1.9.0.min.js"></script>
-  <script async src="//platform.vine.co/static/scripts/embed.js" charset="utf-8"></script>
-
-
   <!-- endbuild -->
+
 </body>
 </html>
