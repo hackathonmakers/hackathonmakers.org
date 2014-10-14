86c83d8563afdee44770661b04ff5a2ff453a470
Martin Rabaglia <tinchoforever@gmail.com>
2014-10-13 21:19:52 -0300

    monster bumb

---
+++ public/apps/hackaton-makers/app.js
@@ -0,0 +1,27 @@
+'use strict';
+
+angular.module('hm-app.controllers', []);
+angular.module('hm-app.directives', []);
+angular.module('hm-app.services', []);
+angular.module('hm-app.factories', []);
+angular.module('hm-app.fiters', []);
+
+// Declare app level module which depends on filters, and services
+angular.module('hm-app', [
+  //'hm-app.filters',
+  //'hm-app.services',
+  //'hm-app.factories',
+  //'hm-app.directives',
+  'hm-app.controllers',
+  'ngRoute',
+]).config(function ($routeProvider) {
+    $routeProvider
+      .when('/', {
+        templateUrl: '/apps/hackaton-makers/views/main.html',
+        controller: 'main'
+      })
+      .otherwise({
+        redirectTo: '/'
+      });
+  });
+
---
+++ public/apps/hackaton-makers/controllers/main.js
@@ -0,0 +1,7 @@
+'use strict';
+angular.module('hm-app.controllers')
+.controller('main', function ($scope) {
+     
+});
+
+
