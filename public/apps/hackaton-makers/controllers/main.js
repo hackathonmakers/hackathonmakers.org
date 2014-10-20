c0a6e2981fa1326eb7939436b28c41a96bf940c0
Martin Rabaglia <tinchoforever@gmail.com>
2014-10-20 01:04:18 -0300

    altas mejoras

--- public/apps/hackaton-makers/controllers/main.js
+++ public/apps/hackaton-makers/controllers/main.js
@@ -1,7 +1,9 @@
 'use strict';
 angular.module('hm-app.controllers')
 .controller('main', function ($scope) {
-     
+    $scope.next = function(id){
+      $('html,body').animate({ scrollTop: $('#'+ id).offset().top }, 'slow');    
+    }  
 });
 
 
